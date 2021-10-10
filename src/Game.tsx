56dd1695d18cbe3f57cc7924b437cbc2cde03a79
wutchara <sstmuham@gmail.com>
2021-10-10 17:10:07 +0700

    Add logic template for this game

--- src/Game.tsx
+++ src/Game.tsx
@@ -17,7 +17,10 @@ import './Game.css';
 interface IGameState {
   time: Date;
   userCount: number;
+  userFailed: number;
   cardListKeys: string[];
+  selectedCard: boolean[];
+  prevSelect: number | null;
 }
 
 interface IGameProps {}
@@ -30,7 +33,6 @@ const cardLists = {
   CoffeeIcon,
   BuildCircleIcon
 };
-console.log('cardLists', cardLists);
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
@@ -46,13 +48,16 @@ export default class Game extends Component<{}, IGameState> {
 
   setup() {
     const cardListKeys = Object.keys(cardLists);
-    console.log('cardListKeys', cardListKeys);
+    const allCards = [...cardListKeys, ...cardListKeys];
 
     this.setState({
       ...this.state,
       time: new Date(),
       userCount: 0,
-      cardListKeys: this.shuffleCard([...cardListKeys, ...cardListKeys]),
+      userFailed: 0,
+      cardListKeys: this.shuffleCard(allCards),
+      selectedCard: allCards.map(() => false),
+      prevSelect: null,
     });
   }
 
@@ -98,37 +103,86 @@ export default class Game extends Component<{}, IGameState> {
   getIcon(key: string) {
     switch (key){
       case 'AddModeratorIcon':
-        return <AddModeratorIcon />
+        return <AddModeratorIcon className="card-icon-front" />
       case 'ArchiveIcon':
-        return <ArchiveIcon />;
+        return <ArchiveIcon className="card-icon-front" />;
       case 'AndroidIcon':
-        return <AndroidIcon />;
+        return <AndroidIcon className="card-icon-front" />;
       case 'CloudUploadIcon':
-        return <CloudUploadIcon />;
+        return <CloudUploadIcon className="card-icon-front" />;
       case 'CoffeeIcon':
-        return <CoffeeIcon />;
+        return <CoffeeIcon className="card-icon-front" />;
       case 'BuildCircleIcon':
-        return <BuildCircleIcon />;
+        return <BuildCircleIcon className="card-icon-front" />;
       }
     }
+
+  selectedCard(key: string, index: number) {
+    const temp = this.state.selectedCard;
+    temp[index] = true;
+    let userCount = this.state.userCount;
+    let userFailed = this.state.userFailed;
+
+    // open cards
+    this.setState({
+      ...this.state,
+      selectedCard: temp,
+    });
+
+    // check matched
+    if (this.state.prevSelect !== null) {
+      if (this.state.cardListKeys[this.state.prevSelect] === this.state.cardListKeys[index]) {
+        userCount = userCount + 1;
+      } else {
+        const previousIndex = this.state.prevSelect;
+        userFailed = userFailed + 1;
+
+        // close 2 cards
+        setTimeout(() => {
+          temp[previousIndex] = false;
+          temp[index] = false;
+        }, 300);
+      }
+    }
+
+    this.setState({
+      ...this.state,
+      selectedCard: temp,
+      userCount,
+      userFailed,
+      prevSelect: this.state.prevSelect === null ? index : null,
+    });
+  }
 
   // render will know everything!
   render() {
     return (
       <div id="game-area" className="container">
-        <p>The current time is {this.state.time.toLocaleTimeString()}</p>
-        <p>userCount: {this.state.userCount}</p>
-        <p>cardLists: {this.state.cardListKeys.toString()}</p>
-        <Button variant="contained">Hello World</Button>
+        <Grid container spacing={4} className="head">
+          <Grid item md={12}><h1>Time: {this.state.time.toLocaleTimeString()}</h1></Grid>
+          <Grid item md={4}><h3>userCount: {this.state.userCount}</h3></Grid>
+          <Grid item md={4}><h3>userFailed: {this.state.userFailed}</h3></Grid>
+          <Grid item md={4}><Button variant="outlined" onClick={() => this.setup()}>Reset</Button></Grid>
+        </Grid>
         <Grid container spacing={3}>
           {this.state.cardListKeys.map((k, i) => {
             return <Grid item md={3} key={k + '-' + i}>
               <Item>
-                <Card sx={{ maxWidth: 345 }}>
+                {/* <Card sx={{ maxWidth: 345 }}>
                   <CardContent>
                     {this.getIcon(k)}
                   </CardContent>
-                </Card>
+                </Card> */}
+                <div className="flip-card">
+                  <div className={`flip-card-inner ${this.state.selectedCard[i] ? "card-selected card-matched" : ""}`} onClick={() => this.selectedCard(k, i)}>
+                    <div className="flip-card-front">
+                      {this.getIcon(k)}
+                    </div>
+                    <div className="flip-card-back">
+                      <img src={'./images/GitHub-Mark-Light-120px-plus.png'} alt="def" />
+                    </div>
+                  </div>
+                </div>
               </Item>
             </Grid>;
           })}
